c7696ceec8cbb81c2b9d8e8e392503694c21f449
randomdude583 <alex@hiddenquarter.com>
2022-12-08 18:50:29 -0600

    Initial commit. Creates docker compose. Creates SQL db and docker scripts.

---
+++ docker/init/mysql/2022-12-04.00.schema.oughtabee.create.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS oughtabee;
+
+CREATE DATABASE oughtabee CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+
+USE `oughtabee`;
+
+DROP TABLE IF EXISTS `pin`;
+CREATE TABLE `pin` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `title` varchar(250) NOT NULL,
+    `location` point NOT NULL,
+    `json_data` json NOT NULL,
+    `created_by` bigint(20) NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` datetime NULL,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`json_data` json NOT NULL,
+	`created_by` bigint(20) NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `deleted_at` datetime NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `json_data` json NOT NULL,
+    PRIMARY KEY (id)
+)
